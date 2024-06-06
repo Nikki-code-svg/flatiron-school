de7afed028b9ba9270488a6737fd8662205f5b41
nicole wachter <nicole.wachter6@gmail.com>
2024-06-06 11:43:52 -0400

    Adds changes

--- phase-1/events/introduction/beginning/index.js
+++ phase-1/events/introduction/beginning/index.js
@@ -17,8 +17,30 @@ Takeaways
 */
 
 // ~ the "click" event
-
+document.querySelector("#tomorrow").addEventListener("click", () => {
+// Here, we decide what should happen when the "click" event fires on #tomorrow.
+document.querySelector("#forever").textContent = "On June 5, 2024.";
+//console.log(event);
+});
 // ~ the "submit" event
+//submitting a form.
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    // event === "submit"
+    //event.target == <form>
+    // event.target["date-inout"] === <input name="date-input">
+    //event.target["date-input"].value === 2024-06-27
+    console.log(event.target["date-input"].value);
+    document.querySelector("#sometime").textContent = event.target["date-input"].value;
+    //event.target is where the event happened on the page.
+})
+//const school = {
+   // name: "flatiron",
+    //cohortSize: 8
+
+//console.log(school.name);
+//console.log(school["name"])
+
 
 // ~ challenges
 // 1. [easy] Add another click event handler.
